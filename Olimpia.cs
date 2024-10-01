b488b1cd29cd9bf56e6c97383349310e02106cac
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-10-01 14:43:43 +0200

    Add project files.

---
+++ Olimpia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paralimpikon
+{
+    internal class Olimpia
+    {
+        public int Ev {  get; set; }
+        public string Orszag { get; set; }
+        public string Varos { get; set; }
+        public int Arany { get; set; }
+        public int Ezust { get; set; }
+        public int Bronz { get; set; }
+
+        public Olimpia(string sor) {
+            var t = sor.Split(';');
+            Ev = int.Parse(t[0]);
+            Orszag = t[1];
+            Varos = t[2];
+            Arany = int.Parse(t[3]);
+            Ezust = int.Parse(t[4]);
+            Bronz = int.Parse(t[5]);
+        }
+
+        public override string ToString()
+        {
+            return $"\tÉv:\t\t{Ev}\n\tOrszág:\t\t{Orszag}\n \tVáros:\t\t{Varos}\n\tEredmények:\tArany: {Arany} | Ezüst: {Ezust} | Bronz: {Bronz}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Paralimpikon;
+using System.Text;
+
+List<Olimpia> olimpiak = new();
+using StreamReader sr = new(
+    path: @"..\..\..\src\paralympics.txt",
+    encoding: UTF8Encoding.UTF8);
+while (!sr.EndOfStream) olimpiak.Add(new(sr.ReadLine()));
+Console.WriteLine($"1. feladat: A Paralimpiát eddig {olimpiak.Count} alkalommal került megrendezésre");
+int f2  = 0;
+foreach (var erem in olimpiak)
+{
+    f2 += erem.Arany;
+    f2 += erem.Ezust;
+    f2 += erem.Bronz;
+}
+Console.WriteLine($"2. feladat: A magyar sportolók összesen {f2} darab érmet szereztek") ;
+Console.WriteLine("3. feladat: A következő helyszíneken nem képviselte magát Magyarország");
+var f3 = olimpiak.Where(o => o.Arany.Equals(0) && o.Ezust.Equals(0) && o.Bronz.Equals(0));
+foreach (var o in f3) Console.WriteLine($"\t{o.Varos} ({o.Ev})");
+
+Console.WriteLine("4. feladat: Országok ami több paralimpiát is rendeztek");
+var f4 = olimpiak.GroupBy(o => o.Orszag).Where(o => o.Count() > 1);
+foreach (var item in f4) Console.WriteLine($"\t{item.Key} ({item.Count()} alk.)");
+
+Console.Write("5. feladat: Írjon be egy évszámot:  ");
+bool val = int.TryParse(Console.ReadLine(), out int y);
+if ( !val ) 
+{
+    Console.WriteLine("Hibás számformátum");
+    return;
+}
+var f5 = olimpiak.SingleOrDefault(o => o.Ev == y);
+if(f5 is null)
+{
+    Console.WriteLine($"{y}-ban nem volt paralimpia");
+    return;
+}
+Console.WriteLine(f5);
